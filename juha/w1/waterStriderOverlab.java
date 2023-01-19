4e33b2488516eb50e3970010a6581a9b3c96928f
Holdm2t1ght <105307294+Holdm2t1ght@users.noreply.github.com>
2023-01-19 15:26:08 +0900

    commit: waterStriderOverlab Sol close #13

---
+++ juha/w1/waterStriderOverlab.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.lang.*;
+
+
+public class waterStriderOverlab {
+
+	static int[] dr = {1, 0};
+	static int[] dc = {0, 1};
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+		
+		for(int test_case = 1;test_case<=T;test_case++) {
+			int n, ws;
+			n = sc.nextInt();
+			ws = sc.nextInt();
+			int[][] arr = new int[n][n];
+			int wsNum = 0;
+				
+			for(int i = 0;i<ws;i++) {
+				int a, b, dir;
+				a = sc.nextInt();
+				b = sc.nextInt();
+				arr[a][b]++;
+				if(arr[a][b]>1) wsNum = i+1;
+				
+				
+				dir = sc.nextInt();
+				int r = a, c = b;
+				for(int j = 3;j>=1;j--) {
+					r += dr[dir-1]*j;
+					c += dc[dir-1]*j;
+					
+					if(r>=0 && r<arr.length && c>=0 && c<arr[a].length) {
+						arr[r][c]++;
+						if(arr[r][c]>1) wsNum = i+1;
+					}
+					else break;
+				}
+				
+			}
+			for(int i = 0;i<n;i++) {
+				for(int j = 0;j<n;j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+				System.out.println();
+			}
+			
+			System.out.println("#" + test_case + " " + wsNum);
+		}
+	}
+
+}
