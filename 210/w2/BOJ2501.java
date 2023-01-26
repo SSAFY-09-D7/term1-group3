eb97dbecaa1dba0248659081af898aa4bbc7fa7f
210-reverof <sheltonwon@gmail.com>
2023-01-26 09:24:01 +0900

    feat: BOJ 2501 약수 구하기 해결 close #59

---
+++ 210/w2/BOJ2501.java
@@ -0,0 +1,23 @@
+package w2;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+        public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("./inputs/input_BOJ2501.txt"));
+        int n = sc.nextInt();
+        int m = sc.nextInt() - 1;
+        List<Integer> nums = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                nums.add(i);
+            }
+        }
+
+        int result = m >= nums.size() ? 0 : nums.get(m);
+        System.out.println(nums.get(0) + " " + result);
+        sc.close();
+    }
+}
