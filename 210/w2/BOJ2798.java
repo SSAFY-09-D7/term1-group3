95ee69bbacc8dcbc172d7d0d02c8f08f8d7b7573
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-01-28 13:35:36 +0900

    feat: BOJ 2798 블랙잭 해결 close #84

---
+++ 210/w2/BOJ2798.java
@@ -0,0 +1,37 @@
+package w2;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("./inputs/input_BOJ2798.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ", false);
+
+        int cnt = Integer.parseInt(st.nextToken());
+        int num = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[cnt];
+        st = new StringTokenizer(br.readLine(), " ", false);
+
+        for (int i = 0 ; i < cnt ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        for (int i = 0;i<cnt;i++){
+            for (int j = i+1; j< cnt; j++) {   
+                for(int k = j+1; k <cnt; k++) {
+                    int tmp = arr[i] + arr[j] + arr[k];
+                    if (tmp > result && tmp <= num) { result = tmp;}
+                }
+            }
+        }
+
+        br.close();
+        bw.write(result + "");
+        bw.close();
+    }
+}
