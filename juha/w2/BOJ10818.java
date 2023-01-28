16b658c31ed23c8f6058f8153ed46f588cd01f5a
Holdm2t1ght <105307294+Holdm2t1ght@users.noreply.github.com>
2023-01-28 21:30:29 +0900

    commit: BOJ 10818 최대, 최소 Sol close #83

---
+++ juha/w2/BOJ10818.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+
+public class minMax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] + " " + arr[arr.length-1]);
+	}
+
+}
