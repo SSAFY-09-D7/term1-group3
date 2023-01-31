1e0e2a5a3e014c44d4226652dc68dda2c1b7937f
210-reverof <sheltonwon@gmail.com>
2023-01-31 09:19:03 +0900

    feat: BOJ 10870 피보나치 수 5 해결 close #100

---
+++ 210/w3/BOJ10870.java
@@ -0,0 +1,23 @@
+package w3;
+
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("./inputs/input_BOJ10870.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n + 1];
+
+        nums[0] = 0;
+        if (n > 0) {
+            nums[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                nums[i] = nums[i - 1] + nums[i - 2];
+            }
+        }
+
+        System.out.println(nums[n]);
+    }
+
+}
