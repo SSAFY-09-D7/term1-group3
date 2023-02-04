4b1ab8791f527f69bba541e09af405e59b756a37
sohyun <park.sohyun0430@gmail.com>
2023-02-04 20:41:23 +0900

    refactor : 모듈화

--- sohyun/src/BOJ1978.java
+++ sohyun/src/BOJ1978.java
@@ -9,17 +9,26 @@ public class BOJ1978 {
         while (st.hasMoreTokens()) {
             int number = Integer.parseInt(st.nextToken());
             //소수판별: 특정 숫자 제곱근까지만 약수의 여부를 검증
-            int cnt = 0;
 
-            for (int i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i == 0)
-                    return;
-            }
-            if (number != 1 && cnt == 0)
+            if (number == 2)
                 count++;
+            else if (number == 1)
+                continue;
+            else if (number % 2 == 0)
+                continue;
+            else {
+                count+=findPrime(number,count);
+            }
         }
         System.out.println(count);
 
     }
-    
+
+    private static  int findPrime(int number, int count){
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0)
+                return 0;
+        }
+        return 1;
+    }
 }
