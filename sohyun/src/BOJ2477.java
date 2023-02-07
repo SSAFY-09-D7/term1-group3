cafbe08a900f1d34a742095e9428855793e91374
박소현 <park.sohyun0430@gmail.com>
2023-02-08 00:02:17 +0900

    commit : BOJ 2477 Solution close #165

--- sohyun/src/BOJ2477.java
+++ sohyun/src/BOJ2477.java
@@ -6,6 +6,9 @@ public class BOJ2477 {
         int N = sc.nextInt();
         int height = 0;
         int width = 0;
+        int heightIdx = 0;
+        int widthIdx = 0;
+        
         int[] dir = new int[6];
         int[] cnt = new int[6];
         for(int i=0;i<6;i++){
@@ -14,25 +17,33 @@ public class BOJ2477 {
 
             if(dir[i]==4 || dir[i]==3){
                 // 남,북이면
-                height = Math.max(height,cnt[i]);
+                if (height < cnt[i]) {
+                    height = cnt[i];
+                    heightIdx = i;
+                }
+                
             }
             if(dir[i]==1||dir[i]==2){
                 // 동,서면
-                width = Math.max(width,cnt[i]);
+                if (width < cnt[i]) {
+                    width = cnt[i];
+                    widthIdx = i;
+                }
                  
             }
         }
 
-        //i==i+2 인경우 찾아서 
-        int area = height*width;
-        int sub = 0;
-
-        for(int i=0;i<3;i++){
-            if(dir[i]==dir[i+2] && dir[i+1]==dir[i+3]){
-                sub = cnt[i+2]*cnt[i+1];
-                
-            }
-        }
+        System.out.println(heightIdx);
+        System.out.println(widthIdx);
+        //작은 사각형 구하려고 할 때
+        //hMAxIdx+3 %6
+        //hMinIdx+3 %6
+        //규칙을 못찾았음 ; 이걸로 구하면 깔끔
+        int area = height * width;
+        int hMinIdx = (heightIdx + 3) % 6;
+        int wMinIdx = (widthIdx + 3) % 6;
+        int sub = cnt[wMinIdx] * cnt[hMinIdx];
+        
         System.out.println((area-sub)*N);
     }
 }
