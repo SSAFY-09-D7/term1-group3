88bc49cbd130b49b792142fd4ffaccc0cb27861c
Sohyun043011 <park.sohyun0430@gmail.com>
2023-02-07 11:10:24 +0900

    commit : BOJ 16206 solution close #151

--- sohyun/src/BOJ16206.java
+++ sohyun/src/BOJ16206.java
@@ -11,43 +11,57 @@ public  class BOJ16206 {
         int M = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         List<Integer> roll = new ArrayList<>();
+
+        //rollcake 길이가 10의 배수인 경우 따로 담아줄 배열
+        List<Integer> roll2 = new ArrayList<>();
         
         //케이크 값 넣기
         while (st.hasMoreTokens()) {
-            roll.add(Integer.parseInt(st.nextToken()));
+            int cake = Integer.parseInt(st.nextToken());
+            if(cake%10==0) roll2.add(cake);
+            else roll.add(cake);
         }
-        
-        int count = 0;
-        int rollCnt = 0;
 
-        for (int idx = 0; idx <N; idx++) {
-            int x = roll.get(idx);
-            // System.out.println(num);
-            if (x == 10) {
-                //10이면 걍 넘김
-                rollCnt++;
-            }
-            else if (x < 10) {
-                continue;
-            }
-            else {
-                //10보다 큰 경우
-                while (x > 0 && M>0) {
-                    x = x - 10;
-                    rollCnt++;
-                    M--;
-                   
+        //10의 배수일 경우 작은것부터 고려해야됨 -> 오름차순 정렬
+        Collections.sort(roll2);
 
-                }
-                if (x % 10==0)
-                {
-                    M++;
-                }
-                if (x == 10)
-                    rollCnt++;
+
+        int cut = 0;            //자르는 횟수
+        int rollCnt = 0;       //조각 갯수
+        int result = 0;
+
+        //1. 10의 배수인 경우에 대해서 cut
+        for(int cake : roll2){
+            //최대 횟수 다 자르면 break
+            if(M<=0) break;
+            rollCnt = cake/10;
+            cut = rollCnt-1;
+
+            if(M<cut){
+                //자른 수가 최대횟수보다 많아지면 
+                //최대횟수로 돌려놓고 조각갯수도 최대횟수로 돌림
+                cut = M;
+                rollCnt = cut;
+              
             }
 
+            M-=cut;
+            result+=rollCnt;
         }
-        System.out.println(rollCnt);
+
+        //2. 10의 배수 아닌 경우
+        for(int cake:roll){
+            if(M<=0) break;
+            rollCnt = cake/10;
+            cut = rollCnt;
+
+            if(M<cut){
+                cut = M;
+                rollCnt = cut;
+            }
+            M-=cut;
+            result+=rollCnt;
+        }
+        System.out.println(result);
     }
 }
