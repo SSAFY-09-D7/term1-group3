8fea15d37bf6e43ad59ae37141e1ed321848f99a
박소현 <park.sohyun0430@gmail.com>
2023-02-15 00:30:27 +0900

    fix: 출력 삭제 주석 정리

--- sohyun/src/BOJ1644.java
+++ sohyun/src/BOJ1644.java
@@ -4,15 +4,11 @@ import java.io.*;
 public class BOJ1644 {
 
     public static void main(String[] args) {
-        //주어진 수를 연속된 소수의 합으로 나타낼 수 있는 경우의 수
-
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
         ArrayList<Integer> primeArr = findPrime(N);
-        //0 인경우가 소수
-        System.out.println(primeArr);
-        //primeArr 돌면서 투포인터 사용
+        
         int start = 0;
         int end = 0;
         int sum = 0;
@@ -20,16 +16,14 @@ public class BOJ1644 {
         while (start < primeArr.size()) {
             if (sum == N)
                 count++;
-                
+
             if (sum > N || end == primeArr.size()) {
                 sum -= primeArr.get(start);
                 start++;
             }
             else {
-                
                 sum += primeArr.get(end);
                 end++;
-                
             }
         }
         System.out.println(count);
