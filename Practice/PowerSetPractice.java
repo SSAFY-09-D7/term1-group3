8d6d21180087e280081a9603ccde7e803d81eb51
이원영 <sheltonwon@gmail.com>
2023-03-05 13:06:50 +0900

    feat: add power set practice

---
+++ Practice/PowerSetPractice.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class PowerSetPractice {
+
+	static int[] arr = { 9, 4, 7, 5 };
+
+	public static void main(String[] args) {
+
+		// 부분 집합
+		powerSet();
+	}
+
+	private static void powerSet() {
+
+	}
+}
