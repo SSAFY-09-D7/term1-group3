e8d26a739bc4d8759b48fb0ae3a752812c1f2ec6
Sohyun043011 <park.sohyun0430@gmail.com>
2023-03-15 11:56:49 +0900

    commit : dfs 코드 추가

--- sohyun/src/BOJ11725.java
+++ sohyun/src/BOJ11725.java
@@ -5,15 +5,13 @@ public class BOJ11725 {
 
     static int N;
 
-    static ArrayList[] tree;
+    static ArrayList<Integer>[] tree;
     static int[] parents;
-    static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         tree = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
         parents = new int[N + 1];
         for (int i = 0; i < N + 1; i++) {
             tree[i] = new ArrayList<Integer>();
@@ -29,23 +27,31 @@ public class BOJ11725 {
             tree[e].add(s);
         }
 
-        bfs();
+        dfs(1);
+        // bfs();
         for (int i = 2; i <= N; i++) {
             System.out.println(parents[i]);
+        }
+    }
+
+    private static void dfs(int n) {
+        for (int ch : tree[n]) {
+            if (parents[ch] != 0)
+                continue;
+            parents[ch] = n;
+            dfs(ch);
         }
     }
 
     private static void bfs() {
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
-        visited[1] = true;
         while (!q.isEmpty()) {
             int p = q.poll();
             for (int j = 0; j < tree[p].size(); j++) {
                 int ch = (int) tree[p].get(j);
-                if (!visited[ch]) {
+                if (parents[ch] == 0) {
                     parents[ch] = p;
-                    visited[ch] = true;
                     q.add(ch);
                 }
             }
