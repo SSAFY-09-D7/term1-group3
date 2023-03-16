fcccda49726152aeef52b527875ad2aedaad0cd7
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-03-16 20:22:41 +0900

    feat: PROGRAMMERS 압축 해결 close #344

---
+++ 210/w9/PROGRAMMERS압축.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 1000;
+        if (s.length ==1) return 1;
+        
+        for(int i = 1; i < s.length / 2 + 1; i++){
+            String str = new String();
+            String c = s.substring(0, i);
+            int cnt = 1;
+            
+            for(int j = i; j < s.length;j += i){
+                if(j+i>len){
+                    if(cnt > 1) str+=cnt+comp;
+                    else str+=comp;
+                    c = s.substring(j);
+                    cnt=1;
+                    continue;
+                }
+                else if(c.equals(s.substring(j, j + i))) cnt++;
+                else {
+                    if(cnt > 1) str += cnt + comp;
+                    else str += c;
+                    c = s.substring(j, j + i);
+                    cnt = 1;
+                }
+            }
+            
+            if (cnt > 1) str += (cnt + c);
+            else str += c;
+            
+            if (answer > str.length()) answer = str.length();
+        }
+        return answer;
+    }
+}
