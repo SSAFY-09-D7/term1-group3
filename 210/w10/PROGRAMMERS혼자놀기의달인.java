5c90c21f0d7b8db09f33303483a84ca4f25f7520
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-03-21 15:26:57 +0900

    feat: PROGRAMMERS 혼자 놀기의 달인 해결 close #351

--- 210/w10/PROGRAMMERS혼자놀기의달인.java
+++ 210/w10/PROGRAMMERS혼자놀기의달인.java
@@ -1,1 +1,37 @@
+import java.util.*;
+import java.util.stream.*;
 
+class Solution {
+
+    public int solution(int[] cards) {
+
+        boolean[] visited = new boolean[cards.length];
+        List<Integer> group = new ArrayList<>();
+
+        for (int i = 0; i < cards.length; i++) {
+            int cnt = 0;
+            int k = i;
+            while (true) {
+                if (!visited[k]) {
+                    visited[k] = true;
+                    k = cards[k] - 1;
+                    cnt++;
+                } else {
+                    if (cnt != 0) group.add(cnt);
+                    break;
+                }
+            }
+        }
+
+        if (group.size() == 1) {
+            return 0;
+        }
+        
+        Collections.sort(group);
+        Collections.reverse(group);
+        return group.get(0) * group.get(1);
+
+
+    }
+
+}
