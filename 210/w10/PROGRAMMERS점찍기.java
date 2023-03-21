c34ea1138e430f752e5ba721d0a5f9b701f53371
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-03-21 09:08:45 +0900

    feat: PROGRAMMERS 점 찍기 해결 close #350

---
+++ 210/w10/PROGRAMMERS점찍기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        double powD = Math.pow(d, 2);
+        
+        for (int y = 0; y <= d; y += k) {
+            double currXMax = Math.sqrt(powD - Math.pow(y, 2));
+            answer += Math.ceil(currXMax / k) + (currXMax % k == 0 ? 1 : 0);
+        }
+
+        return answer;
+    }
+}
