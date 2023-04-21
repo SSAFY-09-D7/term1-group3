30931ccc7a31c3db3dfe168a1a988105af2d4e10
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-04-21 11:46:57 +0900

    fix: PROGRAMMERS 보석 쇼핑 fix #391
    효율성 통과

--- 210/w11/PROGRAMMERS보석쇼핑.java
+++ 210/w11/PROGRAMMERS보석쇼핑.java
@@ -2,37 +2,51 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = {1, 1};
+        int[] answer = {1, gems.length};
         int minLength = Integer.MAX_VALUE;
-        Map<String, Integer> sel = new HashMap<>();
+        int targetCnt = -1;
         Set<String> set = new HashSet<>();
+        Map<String, Integer> map = new HashMap<>();
         
         for (String str : gems)
             set.add(str);
-        int needs = set.size();
         
-        int left = 0, right = 0;
+        targetCnt = set.size();
         
-        while (true) {
-            if (sel.size() == needs) {
-                if (sel.get(gems[left]) > 1) {
-                    sel.put(gems[left], sel.get(gems[left]) - 1);
-                } else sel.remove(gems[left]);
+        int left = 0;
+        int right = 1;
+        map.put(gems[0], 1);
+        while(left < right) { 
+            if (right == gems.length) {                
+                if (map.size() == targetCnt && right - left < minLength) {
+                    minLength = right - left;
+                    answer[0] = left + 1;
+                    answer[1] = right;
+                }
                 
+                if (map.get(gems[left]) == 1) map.remove(gems[left]);
+                else map.put(gems[left], map.get(gems[left]) - 1);
+                
+                left++;
+                continue;
+            }
+            
+            if (map.size() == targetCnt) {
+                if (right - left < minLength) {
+                    minLength = right - left;
+                    answer[0] = left + 1;
+                    answer[1] = right;
+                }
+            
+                if (map.get(gems[left]) == 1) map.remove(gems[left]);
+                else map.put(gems[left], map.get(gems[left]) - 1);
                 left++;
             }
             
-            else if (right < gems.length - 1) {
+            if (map.size() < targetCnt) {
+                map.put(gems[right], map.getOrDefault(gems[right], 0) + 1);
                 right++;
-                sel.put(gems[right], sel.getOrDefault(gems[right], 0) + 1);
-            }
-            
-            else break;
-            
-            if (sel.size() == needs && Math.abs(right - left) < minLength) {
-                answer[0] = left + 1;
-                answer[1] = right + 1;
-                minLength = Math.abs(right - left);
+                continue;
             }
         }
         
